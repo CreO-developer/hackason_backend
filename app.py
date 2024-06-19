33be924ef61d48b020e4d502a32358d07fa4261c
tora68 <t_noda68@outlook.jp>
2024-06-19 09:12:34 +0900

    主観以外の評価

--- app.py
+++ app.py
@@ -8,21 +8,30 @@ import numpy as np
 import cv2
 import requests
 from io import BytesIO
+import logging
+
+from utils import get_image_from_firebase
+from utils import get_subject_image_path
+from utils import get_percent_from_theme, get_score_num_of_people, detect_people_in_image
 
 # from api.number_of_people import detect_people_in_image
 # FastAPIのインスタンス作成
 app = FastAPI()
 
-# 初期化
+# Firebaseの初期化
 cred = credentials.Certificate('./firebase.json')
-firebase_admin.initialize_app(cred, {"storageBucket": "prehackson22.appspot.com"})
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {"storageBucket": "prehackson22.appspot.com"})
 
 # storageのbucketインスタンスを作成
 bucket = storage.bucket()
 
+# ログの設定
+logging.basicConfig(level=logging.INFO)
+
 class Question(BaseModel):
-    imageUrl: str # 自身で撮った画像のURL
-    themeNumber: int # テーマ番号 1~5が1人用、6~10が2人用、11~15が3,4人用
+    imageUrl: str  # 自身で撮った画像のURL
+    themeNumber: int  # テーマ番号 1~5が1人用、6~10が2人用、11~15が3,4人用
 
 @app.get("/")
 async def root():
@@ -30,7 +39,36 @@ async def root():
 
 # テーマ1（図形）の問題 100点満点
 @app.post("/mock/question1")
-async def submit_score_question1(Question):
+async def submit_score_question1(question: Question):
+    # ここでスコアデータを処理します
+    # 開発者の主観15点満点
+    # 人数 15点満点
+    # はみ出しているか35点満点
+    # 枠がどれだけ埋まっているか35点満点
+    num_of_questions = 1
+    # Firebase Storageから画像をダウンロード
+    image = await get_image_from_firebase(question.imageUrl)
+    if image is None:
+        raise HTTPException(status_code=500, detail="Failed to decode image")
+
+    # 人数によるスコアを取得
+    peaple_score = get_score_num_of_people(image, question.themeNumber)
+
+    # お題の画像を取得
+    theme_image_path = get_subject_image_path(num_of_questions, question.themeNumber)
+    
+    # はみ出している割合と含まれている割合を計算
+    exclude_ratio, include_ratio = get_percent_from_theme(image, theme_image_path)
+
+    include_score = include_ratio * 35
+    exclude_score = (1 - exclude_ratio) * 35
+
+    logging.info(f"hamidashi_ratio: {exclude_ratio}, include_ratio: {include_ratio}")
+
+    return {"includeScore": include_score , "excludeScore": exclude_score, "peopleScore": peaple_score, "originalScore": 15}
+
+@app.post("/question1")
+async def submit_score_question1(question: Question):
     # ここでスコアデータを処理します
     # 開発者の主観15点満点
     # 人数 15点満点
@@ -40,7 +78,7 @@ async def submit_score_question1(Question):
 
 # テーマ2（組体操）の問題 100点満点
 @app.post("/mock/question2")
-async def submit_score_question2(Question):
+async def submit_score_question2(question: Question):
     # ここでスコアデータを処理します
     # 開発者の主観15点満点
     # 人数 15点満点
@@ -50,7 +88,7 @@ async def submit_score_question2(Question):
 
 # テーマ3（芸能人）の問題 150点満点
 @app.post("/mock/question3")
-async def submit_score_question3(Question):
+async def submit_score_question3(question: Question):
     # ここでスコアデータを処理します
     #開発者の主観 20点
     #人数 20点
@@ -61,7 +99,7 @@ async def submit_score_question3(Question):
 
 # テーマ4（アニメ、漫画）の問題 150点満点
 @app.post("/mock/question4")
-async def submit_score_question4(Question):
+async def submit_score_question4(question: Question):
     #ここでスコアデータを処理します
     #開発者の主観20点
     #人数20点
@@ -104,7 +142,7 @@ async def get_image(file_name: str):
         return Response(content=str(http_exc.detail), status_code=http_exc.status_code, media_type='text/plain')
     except Exception as e:
         return Response(content=str(e), status_code=500, media_type='text/plain')
-    
- # ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+# ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
---
+++ utils.py
@@ -0,0 +1,114 @@
+from fastapi import FastAPI, HTTPException, Response
+import firebase_admin
+from firebase_admin import credentials, storage
+from pydantic import BaseModel
+import uvicorn
+from datetime import timedelta
+import numpy as np
+import cv2
+import requests
+from io import BytesIO
+from PIL import Image, ImageOps
+from rembg import remove, new_session
+
+# Firebaseの初期化
+cred = credentials.Certificate('./firebase.json')
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {"storageBucket": "prehackson22.appspot.com"})
+
+bucket = storage.bucket()
+
+# rembgのセッションを新しく作成
+session = new_session("u2net")
+
+async def get_image_from_firebase(image_url):
+    try:
+        blob = bucket.blob(image_url)
+        image_url = blob.generate_signed_url(version='v4', expiration=timedelta(seconds=300), method='GET')
+        response = requests.get(image_url)
+        if response.status_code != 200:
+            return None
+        image_array = np.array(bytearray(response.content), dtype=np.uint8)
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        return image
+    except Exception as e:
+        print(f"Error fetching image from Firebase: {e}")
+        return None
+
+def get_percent_from_theme(image, theme_image_path):
+    # 画像をPIL形式に変換
+    image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    # rembgを使用して背景を透過させる
+    output_image = remove(image_pil, session=session)
+
+    # アルファマットを生成
+    alpha_image = remove(image_pil, session=session, post_process_mask=True, only_mask=True)
+
+    # 透過画像をRGB形式に変換
+    alpha_image_rgb = alpha_image.convert("RGB")
+
+    # PNG画像ファイルのパス
+    image_path = theme_image_path
+
+    # 画像を読み込む
+    image_flactal = Image.open(image_path).convert("L")
+
+    # 透過画像と背景画像を合成するために背景を準備
+    back_im1 = alpha_image_rgb.copy()
+    back_im2 = alpha_image_rgb.copy()
+
+    # 透過画像に白い領域を合成
+    back_im1.paste(image_flactal, (0, 0), image_flactal)
+    back_im2.paste(image_flactal, (0, 0), ImageOps.invert(image_flactal))
+
+    # PIL ImageからNumPy配列に変換
+    image_flactal_np = np.array(image_flactal)
+    back_im1_np = np.array(back_im1)
+    back_im2_np = np.array(back_im2)
+
+    # 白い領域の面積を計算
+    whole_area_of_theme = cv2.countNonZero(image_flactal_np)
+    white_area_of_hamidashi = cv2.countNonZero(cv2.cvtColor(back_im1_np, cv2.COLOR_RGB2GRAY))
+    white_area_of_include = cv2.countNonZero(cv2.cvtColor(back_im2_np, cv2.COLOR_RGB2GRAY))
+
+    # はみ出している割合を計算
+    hamidashi_ratio = (white_area_of_hamidashi / whole_area_of_theme) - 1
+
+    # 含まれている割合を計算
+    include_ratio = white_area_of_include / whole_area_of_theme
+
+    return hamidashi_ratio, include_ratio
+
+def get_subject_image_path(num_of_questions:int, num_of_theme:int):
+    image_path = f"./images/question{num_of_questions}/theme{num_of_theme}.png"
+    return image_path
+
+# 画像から人を検出する関数
+def detect_people_in_image(image):
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+    return len(rects)
+
+def get_score_num_of_people(image, theme_num:int):
+    #  人数を検出
+    number_of_people = detect_people_in_image(image)
+    print(number_of_people)
+
+    # お題によって適切な人数を取得
+    appropriate_number = 0
+    if theme_num >= 1 & theme_num <= 5:
+        appropriate_number = 1
+    elif theme_num >= 6 & theme_num <= 10:
+        appropriate_number = 2
+    elif theme_num >= 11 & theme_num <= 15:
+        appropriate_number = 4
+    else:
+        appropriate_number = 0
+    
+    # 人数によるスコアを取得
+    if num_of_people == appropriate_number:
+        return 15
+    else:  
+        return 15 - (np.abs(num_of_people - appropriate_number) * 5) if 15 - (np.abs(num_of_people - appropriate_number) * 5) > 0 else 0
